574642d8b8dd35321e51b7a2507cf0c11f0b58e9
Brandon Young <btyoung2k@gmail.com>
2020-05-13 21:40:21 -0400

    Adding stuff for ch1

---
+++ ch1.py
@@ -0,0 +1,6 @@
+print('Hello, world!\nWhat is your name?')
+myName = input()
+print('Its good to meet you ' + myName + '\nThe length of your name is: ' + str(len(myName)))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
