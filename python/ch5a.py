c670b68f47e71648d6f35e994ed0e1e03b6eb8d6
LittleKross47 <btyoung2k@gmail.com>
2020-05-26 23:47:35 -0400

    Adding validate position. Adding validate piece. Beginning validate board code.

--- python/ch5a.py
+++ python/ch5a.py
@@ -6,5 +6,51 @@
 # space from '1a' to '8h'; that is, a piece can’t be on space '9z'. The piece names begin with either a 'w' or 'b' to represent white or black, followed by 'pawn', 'knight',
 # 'bishop', 'rook', 'queen', or 'king'. This function should detect when a bug has resulted in an improper chess board.
 
+
+def validatePosition(position):
+    return (
+        position[0:1] in ["1", "2", "3", "4", "5", "6", "7", "8"]
+        and position[1:2] in ["a", "b", "c", "d", "e", "f", "g", "h"]
+        and len(position) == 2
+    )
+
+
+def validatePiece(piece):
+    return piece[0:1] in ["b", "w"] and piece[1:] in [
+        "pawn",
+        "knight",
+        "bishop",
+        "rook",
+        "queen",
+        "king",
+    ]
+
+
+# Requires a dict of dicts
 def isValidChessBoard(board):
+    blackCounter = 0
+    whiteCounter = 0
+    pawnCounter = 0
+    for place, piece in board.items():
+        if validatePosition(place) and validatePiece(piece):
+            if piece[1:] == "b":
+                blackCounter += 1
+            else:
+                whiteCounter += 1
+        else:
+            print(
+                "Invalid position or piece: {"
+                + place
+                + ","
+                + piece
+                + "}, please ensure that all pieces are correct and held within the realm of reality... i.e. the plane of a chess board"
+            )
     return True
+
+
+def main():
+    chessBoard = {"1a": "btext"}
+    isValidChessBoard(chessBoard)
+
+
+main()
